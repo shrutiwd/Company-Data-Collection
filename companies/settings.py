7048508c0b72230ab05d26298f380e0476a426ea
Shruti Deolikar <77058904+shrutiwd@users.noreply.github.com>
2023-01-04 13:00:25 +0530

    Update settings.py

--- companies/settings.py
+++ companies/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'companies.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'company_data',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ''
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
